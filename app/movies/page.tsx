b66da3e9ff76e1d5ae532720d0d21a0e3f32fb7d
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-03 22:19:19 +0700

    develop Movies page and BannerPage component

---
+++ app/movies/page.tsx
@@ -0,0 +1,10 @@
+// Template Sections
+import BannerPage from '../../components/sections/BannerPage';
+
+export default function Movies() {
+  return (
+    <>
+      <BannerPage />
+    </>
+  );
+}
