74fb3965d20463b632c0372ea7cf184165a3edef
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-03 22:04:17 +0700

    fix indentation

--- components/sections/Newsletter.tsx
+++ components/sections/Newsletter.tsx
@@ -2,24 +2,24 @@
 export default function Newsletter() {
   return (
     <section className="newsletter-area newsletter-bg" data-background="/assets/img/bg/newsletter_bg.jpg">
-        <div className="container">
-            <div className="newsletter-inner-wrap">
-                <div className="row align-items-center">
-                    <div className="col-lg-6">
-                        <div className="newsletter-content">
-                            <h4>Trial Start First 30 Days.</h4>
-                            <p>Enter your email to create or restart your membership.</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <form action="#" className="newsletter-form">
-                            <input type="email" required placeholder="Enter your email" />
-                            <button className="btn">get started</button>
-                        </form>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="newsletter-inner-wrap">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="newsletter-content">
+                <h4>Trial Start First 30 Days.</h4>
+                <p>Enter your email to create or restart your membership.</p>
+              </div>
             </div>
+            <div className="col-lg-6">
+              <form action="#" className="newsletter-form">
+                <input type="email" required placeholder="Enter your email" />
+                <button className="btn">get started</button>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
