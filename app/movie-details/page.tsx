b097c32c62585b9ca75098bc9b99116f1ae10d91
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-04 09:57:44 +0700

    develop MovieDetails page

---
+++ app/movie-details/page.tsx
@@ -0,0 +1,12 @@
+// Template Sections
+import MovieDetailsArea from '../../components/sections/MovieDetailsArea';
+import EpisodeArea from '../../components/sections/EpisodeArea';
+
+export default function MovieDetails() {
+  return (
+    <>
+      <MovieDetailsArea />
+      <EpisodeArea />
+    </>
+  );
+}
