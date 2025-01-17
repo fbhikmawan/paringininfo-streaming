89e86520ff1099132dd10f2616ba18b8e4af09f8
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-18 00:10:01 +0700

    add 404 not found error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 export default function NotFound() {
   return (
     <section className="breadcrumb-area breadcrumb-bg" data-background="/assets/img/bg/movie_bg.jpg">
@@ -8,11 +6,6 @@ export default function NotFound() {
           <div className="col-12">
             <div className="breadcrumb-content">
             <h2 className="title">404  |  <span>This page could not be found.</span></h2>
-              <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                  <li className="breadcrumb-item"><Link href="/">Home</Link></li>
-                </ol>
-              </nav>
             </div>
           </div>
         </div>
