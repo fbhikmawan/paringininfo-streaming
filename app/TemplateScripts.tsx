03a72cb02161cbd9b01a7ff5605be5afd59364a8
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-19 22:38:25 +0700

    reposition js file dependencies

--- app/TemplateScripts.tsx
+++ app/TemplateScripts.tsx
@@ -127,9 +127,9 @@ export default function TemplateScripts() {
 
   return (
     <>
+      <Script src="../../assets/js/vendor/jquery-3.6.0.min.js" />
       <Script src="../../assets/js/bootstrap.min.js" />
       <Script src="../../assets/js/popper.min.js" />
-      <Script src="../../assets/js/vendor/jquery-3.6.0.min.js" />
     </>
   );
 }
