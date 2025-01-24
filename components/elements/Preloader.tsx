582fc5feacc0f6a11766fd846ba578854effa40c
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-25 01:10:04 +0700

    unassign as client component; set animation by css;

--- components/elements/Preloader.tsx
+++ components/elements/Preloader.tsx
@@ -1,19 +1,10 @@
-'use client'
-
-import { useEffect, useState } from 'react';
 import Image from "next/image";
 import styles from './Preloader.module.css';
 
 export default function Preloader() {
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    setIsVisible(false);
-  }, []);
-
   return (
     <>
-      <div className={`${styles.preloader} ${!isVisible ? styles.fadeOut : ''}`}>
+      <div className={styles.preloader}>
         <div className={styles.loadingCenter}>
           <div className={styles.loadingCenterAbsolute}>
             <Image src="/assets/img/preloader.svg" alt="Preloader" width={80} height={80} priority />
