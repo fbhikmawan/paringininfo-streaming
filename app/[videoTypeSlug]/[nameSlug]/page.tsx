85a658bfc94598ea5770f4b965630343b6eec762
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-25 02:09:42 +0700

    implement Incremental Static Regeneration (ISR)

--- app/[videoTypeSlug]/[nameSlug]/page.tsx
+++ app/[videoTypeSlug]/[nameSlug]/page.tsx
@@ -7,6 +7,25 @@ import { DataMap } from '../../../types/dataMaps';
 import { fetchData } from '../../../lib/videoDataFetcher';
 import { truncateText } from '../../../lib/functions';
 
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 60
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true // or false, to 404 on unknown paths
+
+export async function generateStaticParams() {
+  const data: DataMap = fetchData(['videosDetail']);
+  if (!data.videosDetail) {
+    return [];
+  }
+  return data.videosDetail.map((videoDetail) => ({
+    params: { videoTypeSlug: videoDetail.type.videoTypeSlug, nameSlug: videoDetail.nameSlug },
+  }));
+}
+
 export async function generateMetadata({
   params,
 }: {
--- app/[videoTypeSlug]/page.tsx
+++ app/[videoTypeSlug]/page.tsx
@@ -6,6 +6,25 @@ import ContentsArea from '../../components/sections/ContentsArea';
 import { DataMap } from '../../types/dataMaps';
 import { fetchData } from '../../lib/videoDataFetcher';
 
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 60
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true // or false, to 404 on unknown paths
+
+export async function generateStaticParams() {
+  const data: DataMap = fetchData(['videoTypes']);
+  if (!data.videoTypes) {
+    return [];
+  }
+  return data.videoTypes.map((videoType) => ({
+    params: { videoTypeSlug: videoType.videoTypeSlug },
+  }));
+}
+
 export default async function VideoTypePage({
   params,
 }: {
