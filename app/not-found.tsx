e3d4f879a03b1ba54284b4cd1e2a79adbdf0d30c
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-01-30 00:39:28 +0700

    fix background image on not found error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function NotFound() {
   return (
     <section className="breadcrumb-area breadcrumb-bg">
-      <Image src="/assets/img/bg/breadcrumb_bg.jpg" alt="breadcrumb" fill style={{ objectFit: 'cover' }} />
+      <Image src="/assets/img/bg/movie_bg.jpg" alt="breadcrumb" fill style={{ objectFit: 'cover' }} />
       <div className="container">
         <div className="row">
           <div className="col-12">
